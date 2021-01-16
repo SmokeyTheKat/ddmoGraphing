5851a55ae8c6dbc3f17c82f69e033cb603a74d3a
= <owr5273@gmail.com>
2021-01-16 10:59:44 -0400

    changes

---
+++ js/scripts/publics.js
@@ -0,0 +1,22 @@
+let width = 1920;
+let height = 1079;
+let gwidth = 1920;
+let gheight = 1080;
+
+let tilewidth = 30;
+let tw = tilewidth;
+
+let lines = [];
+let lineColors = [];
+
+let inputs = [];
+let colorPickers = [];
+let newinput = null;
+
+let cam = new v2(0,0);
+let zoom = 1;
+
+let clickPoint = null;
+
+var s = 100;
+var itrs = 20;
